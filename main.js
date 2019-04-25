70736fe4e376474d279ad5feb69c13985449c29e
David Johnson <davidjohnson1297@gmail.com>
2019-04-25 11:19:02 -0700

    small updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-mapboxgl.accessToken = 'pk.eyJ1IjoibW1uZWlzbmVyIiwiYSI6ImNqdW83ZXp2ZjA5dDU0M2xqdXU0MXppbDAifQ.cFQxy45uArJw8aJfOLIQcw';
+// window.onload = window.scrollTo(0, 0)
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
@@ -15,17 +15,43 @@ function displayBeerResults(responseJson) {
           console.log(responseJson[i])
           $('.beerList').append(`
                 <li>
-                  <h3>
-                    <a href="${responseJson[i].website_url}" target="_blank">${responseJson[i].name}</a>
-                  </h3>
-                  <p>${responseJson[i].brewery_type}</p>
-                  <p>${responseJson[i].city}</p>
+                    <h3>
+                      ${responseJson[i].name}
+                    </h3>
+                    <p>
+                      ${responseJson[i].brewery_type}
+                    </p>
+                    <address>
+                      ${responseJson[i].city}, ${responseJson[i].state} ${responseJson[i].postal_code}</br>
+                      ${responseJson[i].street} </br>
+                      ${responseJson[i].phone}
+                    </address>
+                    <a href="${responseJson[i].website_url}" target="_blank">
+                      ${responseJson[i].website_url}
+                    </a>
                 </li>`
            );
         }
     }
 }
+/*
+brewery_type: "brewpub"
+city: "Chicago"
+country: "United States"
+id: 2420
+latitude: "41.70388555"
+longitude: "-87.6819511632652"
+name: "Horse Thief Hollow Brewery"
+phone: "7737792739"
+postal_code: "60643-2508"
+state: "Illinois"
+street: "10426 S Western Ave"
+tag_list: []
+updated_at: "2018-08-24T00:31:16.933Z"
+website_url: "http://www.horsethiefbrewing.com"
+__proto__: Object
 
+*/
 function getBeer (searchUrl, fullName) {
     // Setting up parameters
     const params = {
