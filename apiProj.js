90407d020d5aade6fec7c66f623131f1f1c0cb6c
David Johnson <davidjohnson1297@gmail.com>
2019-04-30 15:07:14 -0700

    changes from today

--- apiProj.js
+++ apiProj.js
@@ -1,5 +1,5 @@
 'use strict';
-// window.onload = window.scrollTo(0, 0)
+window.onload = window.scrollTo(0, 0)
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
@@ -13,45 +13,29 @@ function displayBeerResults(responseJson) {
     for (let i = 0; i < responseJson.length; i++) {
         if (responseJson[i].brewery_type != 'planning') {
           console.log(responseJson[i])
+          $('.second').css('opacity', '1')
           $('.beerList').append(`
-                <li>
-                    <h3>
-                      ${responseJson[i].name}
-                    </h3>
-                    <p>
-                      ${responseJson[i].brewery_type}
-                    </p>
-                    <address>
-                      ${responseJson[i].city}, ${responseJson[i].state} ${responseJson[i].postal_code}</br>
-                      ${responseJson[i].street} </br>
-                      ${responseJson[i].phone}
-                    </address>
-                    <a href="${responseJson[i].website_url}" target="_blank">
-                      ${responseJson[i].website_url}
-                    </a>
-                </li>`
+          <li>
+              <h3 class="name">
+                ${responseJson[i].name}
+              </h3>
+              <p>
+                ${responseJson[i].brewery_type}
+              </p>
+              <address>
+                ${responseJson[i].city}, ${responseJson[i].state} ${responseJson[i].postal_code}</br>
+                ${responseJson[i].street} </br>
+                ${responseJson[i].phone}
+              </address>
+              <a href="${responseJson[i].website_url}" target="_blank">
+                ${responseJson[i].website_url}
+              </a>
+          </li>`
            );
         }
     }
 }
-/*
-brewery_type: "brewpub"
-city: "Chicago"
-country: "United States"
-id: 2420
-latitude: "41.70388555"
-longitude: "-87.6819511632652"
-name: "Horse Thief Hollow Brewery"
-phone: "7737792739"
-postal_code: "60643-2508"
-state: "Illinois"
-street: "10426 S Western Ave"
-tag_list: []
-updated_at: "2018-08-24T00:31:16.933Z"
-website_url: "http://www.horsethiefbrewing.com"
-__proto__: Object
 
-*/
 function getBeer (searchUrl, fullName) {
     // Setting up parameters
     const params = {
@@ -88,9 +72,3 @@ $('.getBeer').on('click', function(event) {
   getBeer(searchURL, fullName);
   //load map goes here!!
 });
-
-
-//var map = new mapboxgl.Map({
-  //  container: 'mapBox',
-    //style: //'mapbox://styles/mmneisner/cjuoba9ua0p6k1flc33sds3o8'
-//});
